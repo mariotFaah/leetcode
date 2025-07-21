9fe0fcfb4e096c6346f1b186d679c9410ad555fd
mariotFaah <mariotfanantenana@gmail.com>
2025-07-21 22:29:47 +0300

    22:29 21 Juillet 2025

---
+++ searchInsert.py
@@ -0,0 +1,18 @@
+'''
+Binary search comme d'hab
+'''
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) -1
+        while left < right:
+            middle = (left+right)//2
+            if targt == nums[middle]:
+                return middle
+            else if target > nums[mid]:
+                left = middle + 1
+            else:
+                right = middle - 1
+        return left
+

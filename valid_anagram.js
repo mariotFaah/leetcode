39884c4e9645d0edff6e88027305a141217be44e
mariotFaah <mariotfanantenana@gmail.com>
2025-07-30 21:02:34 +0300

    30 juillet 2025

---
+++ valid_anagram.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if(s.length !== t.length) return false;
+    let map = {}
+
+    for(let i=0; i<s.length; i++){
+        let letter = s[i];
+
+        if(!map[letter]){
+            map[letter] = 1;
+        }else {
+            map[letter]++;
+        }
+    }
+    for(let i=0; i<t.length; i++){
+        let letter = t[i];
+        if(map[letter] === undefined){
+            return false;
+        }
+        if(map[letter]<1){
+            return false;
+        }
+        map[letter]--;
+    }
+    return true;
+};
